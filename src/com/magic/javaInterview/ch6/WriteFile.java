4661154a72cc5b6c44907f77cbb1a1ffa6cc1a51
Magicfeng007 <xzf1108@foxmail.com>
2018-01-25 21:58:37 +0800

    add writeStringToFIleByFileOutputStream method

--- src/com/magic/javaInterview/ch6/WriteFile.java
+++ src/com/magic/javaInterview/ch6/WriteFile.java
@@ -1,23 +1,21 @@
 package com.magic.javaInterview.ch6;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class WriteFile {
     public static void main(String[] args){
         WriteFile writeFile = new WriteFile();
-        writeFile.writeStringToFile("testWriteStringToFile","/home/magic/test/testWrite/");
+//        writeFile.writeStringToFileByFileWriter("testWriteStringToFile","/home/magic/test/testWrite/");
+        writeFile.writeStringToFIleByFileOutputStream("testWriteStringToFile","/home/magic/test/testWrite/","myfile.txt");
     }
 
-    public void writeStringToFile(String str,String filePath){
+    public void writeStringToFileByFileWriter(String str,String filePath,String fileName){
         File file = new File(filePath);
         BufferedWriter bufferedWriter = null;
         try{
             if (file.exists()){
                 if(file.isDirectory()){
-                    File targetFile = new File(file.getPath() + "/myfile.txt");
+                    File targetFile = new File(file.getPath() + File.separator + fileName);
                     if(targetFile.exists() && targetFile.isFile()){
                         bufferedWriter = new BufferedWriter(new FileWriter(targetFile,true));
                         bufferedWriter.newLine();
@@ -25,7 +23,7 @@ public class WriteFile {
                         bufferedWriter.flush();
                         bufferedWriter.close();
                     }else {
-                        bufferedWriter = new BufferedWriter(new FileWriter(file.getPath() + "/myfile.txt"));
+                        bufferedWriter = new BufferedWriter(new FileWriter(file.getPath() + File.separator + fileName));
                         bufferedWriter.write(str);
                         bufferedWriter.flush();
                         bufferedWriter.close();
@@ -33,7 +31,45 @@ public class WriteFile {
                 }
             }else {
                 file.mkdirs();
-                bufferedWriter = new BufferedWriter(new FileWriter(file.getPath() + "/myfile.txt"));
+                bufferedWriter = new BufferedWriter(new FileWriter(file.getPath() + File.separator + fileName));
+                bufferedWriter.write(str);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeStringToFIleByFileOutputStream(String str,String filePath,String fileName){
+        File file = new File(filePath);
+        BufferedWriter bufferedWriter = null;
+        try{
+            if (file.exists()){
+                if(file.isDirectory()){
+                    File targetFile = new File(file.getPath() + File.separator + fileName);
+                    if(targetFile.exists() && targetFile.isFile()){
+                        OutputStream outputStream = new FileOutputStream(file.getPath() + File.separator + fileName,true);
+                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                        bufferedWriter = new BufferedWriter(outputStreamWriter);
+                        bufferedWriter.newLine();
+                        bufferedWriter.append(str);
+                        bufferedWriter.flush();
+                        bufferedWriter.close();
+                    }else {
+                        OutputStream outputStream = new FileOutputStream(file.getPath() + File.separator + fileName);
+                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                        bufferedWriter = new BufferedWriter(outputStreamWriter);
+                        bufferedWriter.write(str);
+                        bufferedWriter.flush();
+                        bufferedWriter.close();
+                    }
+                }
+            }else {
+                file.mkdirs();
+                OutputStream outputStream = new FileOutputStream(file.getPath() + File.separator + fileName);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
                 bufferedWriter.write(str);
                 bufferedWriter.flush();
                 bufferedWriter.close();
