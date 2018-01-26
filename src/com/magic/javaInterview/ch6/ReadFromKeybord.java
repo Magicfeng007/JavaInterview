95bec56a51941c8773d9c2a9f3d1f36ee24c2717
Magicfeng007 <xzf1108@foxmail.com>
2018-01-26 21:55:30 +0800

    使用BufferedReader包装System.in读取输入的字符

--- src/com/magic/javaInterview/ch6/ReadFromKeybord.java
+++ src/com/magic/javaInterview/ch6/ReadFromKeybord.java
@@ -2,7 +2,6 @@ package com.magic.javaInterview.ch6;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class ReadFromKeybord {
